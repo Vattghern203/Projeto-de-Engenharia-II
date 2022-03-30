78e702c39848b9cb8905f35304dac18b1e085495
vattghern <otavioaugustobrb19@gmail.com>
2022-03-30 19:45:02 -0300

    modal done

--- static/script.js
+++ static/script.js
@@ -7,3 +7,7 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+function openForm() {
+  document.getElementById("create-form").style.display = 'block'
+}
