5231fca06f25be4b917f168d52e86721a893b2d8
vattghern <otavioaugustobrb19@gmail.com>
2022-03-31 13:34:11 -0300

    sobre

--- main.py
+++ main.py
@@ -52,6 +52,10 @@ def criar():
 def status():
     return render_template('status.html')
 
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
 
 if __name__ == '__main__':
 
